3b46960e4d75b5b2eee95e887a2eec40f4238bce
oreste-abizera <oresteabizera11@gmail.com>
2024-04-13 12:53:59 +0200

    Add evaluator.py for expression evaluation

---
+++ evaluator.py
@@ -0,0 +1,20 @@
+import math
+from errors import error_handling
+from color import Color, colorful_output
+
+def expression_evaluation(calculator):
+    try:
+        colorful_output("-" * 30, Color.BLUE)
+        colorful_output("Expression Evaluation:", Color.BLUE)
+        colorful_output("-" * 30, Color.BLUE)
+        expression = input("Enter an expression: ")
+        result = evaluate_expression(expression)
+        resultInMessage = f"{expression} = {result}"
+        colorful_output(resultInMessage, Color.GREEN)
+        calculator.history.append(f"{expression} = {result}")
+    
+    except Exception as e:
+        error_handling(str(e))
+        
+def evaluate_expression(expression):
+    return eval(expression)
