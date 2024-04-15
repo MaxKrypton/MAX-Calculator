cf17e0434f3ee16a853488a9e71f43c52a1b44b0
kylaochweri <k.ochweri@alustudent.com>
2024-04-15 09:04:16 -0700

    added information

--- input.py
+++ input.py
@@ -3,3 +3,8 @@ from errors import error_handling
 
 def dynamic_input_handling():
     while True:
+                user_input = input("Enter your choice: ").strip()
+        if user_input.isdigit():
+            return user_input
+        else:
+            error_handling("Please enter a valid number.")
